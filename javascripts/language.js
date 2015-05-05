b1c2a6586f3fe0568871f0a5d4f52294b2c0bc50
刘畅 <lc918.student@sina.com>
2015-05-05 17:53:30 +0800

    update language
    update language choose

---
+++ javascripts/language.js
@@ -0,0 +1,8 @@
+var en = {
+	'title':'xiaobai\'s home - GitHub.io',
+	'subtitle':'long time no see, please call me:+86 13811400337'
+}
+var ch = {
+	'title':'刘小白的私人领土 - GitHub.io',
+	'subtitle':'有事没事多联系 TEL:13811400xxx'
+}
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,11 @@
 console.log('This would be the main JS file.');
+function changeLanguage(lang){
+	//console.log(lang);
+	for (item in lang)
+	{
+		$('.lan-'+item).text(lang[item]);
+	}
+}
+$(function(){
+	changeLanguage(ch);
+})
